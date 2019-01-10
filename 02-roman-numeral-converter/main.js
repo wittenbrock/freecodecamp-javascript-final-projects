de6a0b18dc61da57bd11f9a8d0850e8b64006e52
William Wittenbrock <william.wittenbrock@gmail.com>
2019-01-10 14:16:41 -0800

    Add JavaScript files and solutions

--- 02-roman-numeral-converter/main.js
+++ 02-roman-numeral-converter/main.js
@@ -1,3 +1,72 @@
 /*
+Convert the given number into a roman numeral.
 
+All roman numerals answers should be provided in upper-case.
 */
+
+let convertToRoman = num => {
+  if (num > 3999) { return undefined };
+  let numbers = num.toString().split('').map(el => Number(el));
+  const thousand = {
+      1: "M",
+      2: "MM",
+      3: "MMM"
+  }
+  const hundred = {
+      1: "C",
+      2: "CC",
+      3: "CCC",
+      4: "CD",
+      5: "D",
+      6: "DC",
+      7: "DCC",
+      8: "DCCC",
+      9: "CM"
+  }
+  const ten = {
+      1: "X",
+      2: "XX",
+      3: "XX/////////X",
+      4: "XL",
+      5: "L",
+      6: "LX",
+      7: "LXX",
+      8: "LXXX",
+      9: "XC"
+  }
+  const one = {
+      1: "I",
+      2: "II",
+      3: "III",
+      4: "IV",
+      5: "V",
+      6: "VI",
+      7: "VII",
+      8: "VIII",
+      9: "IX"
+  }
+  let result;
+  if (numbers.length === 4) {
+      let zero = numbers.slice(0,1).map(el => thousand[el]).join("");
+      let first = numbers.slice(1,2).map(el => hundred[el]).join("");
+      let second = numbers.slice(2,3).map(el => ten[el]).join("");
+      let third = numbers.slice(3,).map(el => one[el]).join("");
+      result = zero + first + second + third;
+  }
+  if (numbers.length === 3) {
+      let first = numbers.slice(0,1).map(el => hundred[el]).join("");
+      let second = numbers.slice(1,2).map(el => ten[el]).join("");
+      let third = numbers.slice(2,).map(el => one[el]).join("");
+      result = first + second + third
+  }
+  if (numbers.length === 2) {
+      let second = numbers.slice(0,1).map(el => ten[el]).join("");
+      let third = numbers.slice(1,).map(el => one[el]).join("");
+      result = second + third;
+  }
+  if (numbers.length === 1) {
+      let third = numbers.map(el => one[el]).join("");
+      result = third
+  }
+  return result
+}
--- 05-cash-register/main.js
+++ 05-cash-register/main.js
@@ -1,3 +1,69 @@
 /*
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
 */
+
+let checkCashRegister = (price, cash, cid) => {
+
+  let changeTotal = cash - price;
+  let registerStatus = '';
+
+  let statusCalc = () => {
+      let registerTotal = Number(cid.flat().filter(el => typeof el === 'number').reduce((total, val) => total += val).toFixed(2));
+
+      if (changeTotal < registerTotal) {
+          registerStatus = "OPEN";
+      } else if (changeTotal === registerTotal) {
+          registerStatus = "CLOSED";
+      }
+  }
+
+  let changeCalc = () => {
+      let cidWords = cid.flat().filter(el => typeof el === 'string')
+      let cidNumbers = cid.reverse().flat().filter(el => typeof el === 'number')
+      const moneyUnits = [100, 20, 10, 5, 1, .25, .1, .05, .01];
+      let change = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+          for (let i = 0; i < cidNumbers.length; i++) {
+              if (moneyUnits[i] <= changeTotal) {
+                  while (cidNumbers[i] > 0 && moneyUnits[i] <= changeTotal) {
+                          cidNumbers[i] -= moneyUnits[i]
+                          changeTotal -= moneyUnits[i]
+                          changeTotal = changeTotal.toFixed(2)
+                          change[i] += moneyUnits[i]
+                  }
+              }
+          }
+
+
+          if (Number(changeTotal) !== 0) {
+              registerStatus = "INSUFFICIENT_FUNDS";
+          }
+
+      return change.reverse()
+                   .map(el => el === 0 ? 0 : Number(el.toFixed(2)))
+                   .map((elem, i) => [cidWords[i], elem])
+  }
+
+  statusCalc();
+  let changeClosed = changeCalc();
+
+  let changeOpen = [...changeClosed].reverse().filter(e1 => !e1.includes(0))
+  console.log(registerStatus);
+  if (registerStatus === 'INSUFFICIENT_FUNDS') {
+      return {status: registerStatus, change: []};
+  } else if (registerStatus === 'CLOSED') {
+      return {status: registerStatus, change: changeClosed};
+  } else {
+      return {status: registerStatus, change: changeOpen};
+  }
+}
